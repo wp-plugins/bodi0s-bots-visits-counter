e4227a939654bff89ba8991eadb27387d1fd5f0d
budiony <budiony@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-27 13:50:49 +0000

    New version 0.6
    git-svn-id: https://plugins.svn.wordpress.org/bodi0s-bots-visits-counter/tags/0.6@846252 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bodi0-bot-ajax.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Plugin`s AJAX function calls
+Author: bodi0
+Email: budiony@gmail.com
+Version: 0.6
+License: GPL2
+
+		Copyright 2014  bodi0  (email : budiony@gmail.com)
+		
+		This program is free software; you can redistribute it and/or modify
+		it under the terms of the GNU General Public License, version 2, as 
+		published by the Free Software Foundation.
+		
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+		
+		You should have received a copy of the GNU General Public License
+		along with this program; if not, write to the Free Software
+		Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+//Typical headers
+header('Content-Type: text/html');
+
+//Disable caching
+header('Cache-Control: no-cache');
+header('Pragma: no-cache');
+
+//Action via 'GET'
+$action = (isset( $_GET['action'])) ? $_GET['action'] : '';
+
+//For translations
+require_once ('../../../wp-includes/l10n.php');
+require_once ('../../../wp-includes/pomo/translations.php');
+require_once ('../../../wp-includes/plugin.php');
+
+//A bit of security
+if(!in_array($action, array( 'get_location_info', 'get_pagerank_google', 'get_pagerank_alexa' ))) {
+	_e( 'Invalid AJAX action.' ); 
+	exit();
+}
+//The AJAX action is OK, let`s switch it
+else {  
+	switch ($action) {
+		case 'get_location_info' :
+		get_location_info();
+		break;
+		
+		case 'get_pagerank_google' :
+		$url = isset($_GET['url'])? $_GET['url'] : '';
+		get_pagerank_google($url);
+		break;
+		
+		
+		case 'get_pagerank_alexa' :
+		$url = isset($_GET['url'])? $_GET['url'] : '';
+		get_pagerank_alexa($url);
+		break;
+		
+		default : 
+		break;
+		}
+ }
+
+// Get and display location information via AJAX
+function get_location_info () {
+	require(dirname(__FILE__)."/class.geolocation.php");
+	/*******************************/
+	$html = '';
+	//Class instance
+	$geo = new geo_location;
+	//Get IP
+	$geo->remote = isset($_GET['ip'])? $_GET['ip'] : '';
+	//The info
+	echo '<h4>'.__('Location info').'</h4>';
+	//If is valid IP address
+	if (filter_var($geo->remote, FILTER_VALIDATE_IP )) {
+		foreach ($geo->getLocationInfoByIp() as $key=>$value) {
+			if ($key != 'geoplugin_credit')
+			echo  strip_tags(substr($key,10).': '.(empty($value) ? 'N/A' : '<strong>'.$value.'</strong>'), '<strong>')."<br/>"; 
+		}
+	}
+	else {
+		_e ("Invalid IP address: ".(isset($_GET['ip']) ? $_GET['ip'] : '') );	
+	}
+}
+
+// Get Google`s page rank for given URL
+function get_pagerank_google($url) {
+	// Include the class
+	require(dirname(__FILE__)."/class.google-pagerank.php");
+	//If is valid URL
+	if (filter_var($url, FILTER_VALIDATE_URL )) {
+		// Display result
+		$google = new get_google_pagerank;
+		echo $google->pagerank($url);
+	}
+	else _e("Invalid URL", "bodi0-bot-counter");
+}
+
+// Get Google`s page rank for given URL
+function get_pagerank_alexa($url) {
+	// Include the class
+	require(dirname(__FILE__)."/class.alexa-pagerank.php");
+	//If is valid URL
+	if (filter_var($url, FILTER_VALIDATE_URL )) {
+		// Display result
+		$alexa = new get_alexa_rank();
+		//Get the rank for the domain paulund.co.uk
+		echo $alexa->get_rank($url);
+	}
+	else _e("Invalid URL", "bodi0-bot-counter");
+}
+
+
+//EOF
+?>
---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== bodi0`s Bots visits counter ===
+Contributors:
+Donate link:
+Tags: website, visits, counter, bot, crawler, spider, statistics
+Requires at least: 3.1.0
+Tested up to: 3.8.1
+Stable tag: 0.6
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Counts the visits from web spiders, crawlers and bots in your blog. 
+
+== Description ==
+Counts the visits from web spiders, crawlers and bots in your blog. 
+Also can count any other visit, the plug-in is looking for patterns in user-agent string, which pattern can be customized.
+You can block or unblock crawler, spider (or any other item), by IP address and identifier via .htaccess file.
+
+== Installation ==
+This section describes how to install the plugin and get it working.
+
+1. Upload unzipped archive of the plugin to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+None
+
+== Screenshots ==
+None
+
+== Changelog ==
+= 0.6 =
+Added ability to get the blog rankings, from Google or Alexa.
+Small improvements, translation updated and bugfixes.
+
+=0.5=
+Updated filtering of the results. Now filter applies to any table column.
+Added option to block/unblock bot, by IP address and identifier via .htaccess file, the plugin creates a backup of your old .htaccess file in case something goes wrong, called .htaccess.bot-counter-backup.txt in the same folder as your original file.
+Removed unnecessary javascript files related to table results filtering.
+
+= 0.4 =
+Added in-line edit/delete of bot's name.
+Added ability to export statistics as XML Spreadsheet file, can be opened by MS Excel.
+Added French translation.
+Bugfixes and small improvements in filtering and sorting of the results.
+
+= 0.3 =
+Added on-page sort of the table with results (TableSorter 2.14.4 by Christian Bach).
+
+= 0.2 =
+Added internationalization support. For now Bulgarian and English only.
+Added ability to filter the bot list results.
+Added additional statistics.
+Added geo-location search by bot's IP address.
+Improved plugin's security.
+Various small bug-fixes and improvements.
+
+= 0.1 =
+Initial release.
